61ee81585215322c9fa536c116c9220c42faf6f1
Eduardo José <duduxss3@gmail.com>
2022-12-27 21:11:10 -0300

    :construction_worker: testing ci

--- backend/ms-user/src/main/java/com/edudev/msuser/MsUserApplication.java
+++ backend/ms-user/src/main/java/com/edudev/msuser/MsUserApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MsUserApplication {
 
-	public static void main(String[] args) {SpringApplication.run(MsUserApplication.class, args);}
+    public static void main(String[] args) {
+        SpringApplication.run(MsUserApplication.class, args);
+    }
 
 }
